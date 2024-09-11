81fa48499d7d4f36a1f78716f876debfb8e37adc
kapoQualitest <kapo.tsang@qualitestgroup.com>
2024-09-11 14:08:40 +0100

    first commit

---
+++ features/StepDefinitions/ResponseCode.feature
@@ -0,0 +1,8 @@
+Feature: Response Code
+
+Scenario: 200 Response Recieved 
+Given User has Endpoint URL
+When User makes a GET request
+Then Response should be 200
+
+
---
+++ features/StepDefinitions/ResponseCode.js
@@ -0,0 +1,27 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+const {Given,When,Then} = require('cucumber')
+
+
+  Given('User has Endpoint URL', function () {
+    const request2 = supertest('https://testapi.io/api/RMSTest/ibltest');
+    return console.log("Endpoint ready");
+  });
+
+
+
+  When ('User makes a GET request', function () {
+    return request2.get('ibltest').then((response) => {
+        expect(response.status).to.be.eq(200);
+    return console.log(response.status);
+  });
+});
+
+
+  Then ('Response should be 200', function (int) {
+    if(response.status=200){
+        console.log("Success")
+    }
+
+    return 'pending';
+  });
